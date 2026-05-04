17f0567d921e819f6c14929ff8be771f34df6ee7
Aubrey-projects <daydreamcatcher143@gmail.com>
2026-05-04 17:56:30 -0600

    Finished Step 6 of the prepare assignment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,16 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
-
         Job job1 = new Job();
         job1._company = "Microsoft";
         job1._jobTitle = "Software Engineer";
         job1._startYear = 1988;
         job1._endYear = 2022;
 
-        Console.WriteLine(job1._company);
-        Console.WriteLine(job1._jobTitle);
+        job1.Display();
 
         Job job2 = new Job();
         job2._company = "Apple";
@@ -21,8 +18,7 @@ class Program
         job2._startYear = 2003;
         job2._endYear = 2018;
 
-        Console.WriteLine(job2._company);
-        Console.WriteLine(job2._jobTitle);
+        job2.Display();
         
 
         
