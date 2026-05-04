3958c82870159894f804a0a7557929d22a246416
Aubrey-projects <daydreamcatcher143@gmail.com>
2026-05-04 17:52:55 -0600

    Prepare assignment step 4 complete

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,26 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 1988;
+        job1._endYear = 2022;
+
+        Console.WriteLine(job1._company);
+        Console.WriteLine(job1._jobTitle);
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Manager";
+        job2._startYear = 2003;
+        job2._endYear = 2018;
+
+        Console.WriteLine(job2._company);
+        Console.WriteLine(job2._jobTitle);
+        
+
+        
     }
 }
