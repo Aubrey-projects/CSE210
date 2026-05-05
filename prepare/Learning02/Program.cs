3107601b857aef306b017aa85f031760367cf7e9
Aubrey-projects <daydreamcatcher143@gmail.com>
2026-05-04 18:17:05 -0600

    Finished last step of the prepare assignment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -10,21 +10,18 @@ class Program
         job1._startYear = 1988;
         job1._endYear = 2022;
 
-        job1.Display();
-
         Job job2 = new Job();
         job2._company = "Apple";
         job2._jobTitle = "Manager";
         job2._startYear = 2003;
         job2._endYear = 2018;
 
-        job2.Display();
-
         Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
         myResume._jobs.Add(job1);
         myResume._jobs.Add(job2);
 
-        Console.WriteLine(myResume._jobs[0]._jobTitle);
+        myResume.DisplayJobs();
         
 
         
--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -1,5 +1,16 @@
+using System.Diagnostics.Metrics;
+
 public class Resume
 {
     public string _name;
     public List<Job> _jobs = new List<Job>();
+
+    public void DisplayJobs()
+    {
+        Console.WriteLine($"Name: {_name}\nJobs:");
+        foreach(Job job in _jobs)
+        {
+            job.Display();
+        }
+    }
 }
