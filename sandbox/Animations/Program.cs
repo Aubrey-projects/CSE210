92db08f744fb9daaf1da217275e08b6d7f4e3b62
Aubrey-projects <daydreamcatcher143@gmail.com>
2026-06-10 14:25:45 -0600

    Worked on making animations in class

--- sandbox/Animations/Program.cs
+++ sandbox/Animations/Program.cs
@@ -15,16 +15,33 @@
         //     Console.Write("\b");
         // }
 
-        string animationString = "\\|/|";
-        for (int i = 0; i < 20; i++)
+        string animationString = "\\|/-";
+        DateTime now = DateTime.Now;
+        DateTime endTime = now.AddSeconds(10);
+        int index = 0; // don't need the index if we use the for loop
+        while(DateTime.Now < endTime)
         {
-            Console.Write(animationString[i % animationString.Length]);
+        // for (int i = 0; i < 20; i++)
+        // {
+            Console.Write(animationString[index++ % animationString.Length]);
             Thread.Sleep(sleepTime);
             Console.Write("\b");
             // Console.Write("-");
             // Thread.Sleep(sleepTime);
             // Console.Write("\b");
+        // }
         }
+
+        int count = 15;
+        DateTime now2 = DateTime.Now;
+        DateTime endTime2 = now2.AddSeconds(count);
+        while(DateTime.Now < endTime2)
+        {
+            Console.Write($"{count--,2}");
+            Thread.Sleep(1000);
+            Console.Write("\b\b");
+        }
+
         Console.CursorVisible = true;
         
     }
