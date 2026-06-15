c86e05cb6985ed1273a2c416c59ddde9d4a2a13b
Aubrey-projects <daydreamcatcher143@gmail.com>
2026-06-15 11:06:21 -0600

    Finished the prepare assignment

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> Shapes = new List<Shape>();
+        Square mySquare = new Square("blue", 3);
+        Rectangle myRectangle = new Rectangle("pink", 6, 4);
+        Circle myCircle = new Circle("yellow", 5);
+        Shapes.Add(mySquare);
+        Shapes.Add(myRectangle);
+        Shapes.Add(myCircle);
+        foreach(Shape myShape in Shapes)
+        {
+            Console.WriteLine($"{myShape.GetColor()}, {myShape.GetArea():F2}");
+        }
     }
 }
